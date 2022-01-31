784fd829981773e9837857e660b82b2e31ae1934
gdapian <97063775+gdapian@users.noreply.github.com>
2022-01-31 22:53:46 +0100

    added esu algorithm
    to fix, infinite loop

---
+++ src/motifs.py
@@ -0,0 +1,101 @@
+# https://tobigs.gitbook.io/tobigs-graph-study/chapter3./python-code-roix-and-esu-tree
+def iterative_ESU(G, k):
+  N = len(G)
+  visited = [False] * N
+  queue = []
+  ans = 0
+  sub = {}
+  sub_graph = {}
+  for i in range(N):
+    queue.append([i])
+  while queue:
+    front = queue[0]
+    queue = queue[1:]
+    if len(front)==k:
+      degree = [0] * N
+      for f in front:
+        for j in front:
+          if G[f][j]==1:
+            degree[f] += 1
+            degree[j] += 1
+      if len(set(front)) != len(front):
+        continue
+      if str(sorted(front)) in sub_graph:
+        continue
+      sub_graph[str(sorted(front))] = 1
+      if str(sorted(degree)) in sub:
+        sub[str(sorted(degree))] += 1 
+      else:
+        sub[str(sorted(degree))] = 1
+      print(front)
+      continue
+    for i in range(N):
+      if G[front[-1]][i]==1 and i > front[0]:
+        queue.append(front+[i])
+  for s in sub:
+    ans += sub[s]
+  return ans
+
+#####################################################################
+
+import numpy as np
+
+subgraphs = []
+
+def EnumerateSubgraphs(G, k):
+    n = len(G)
+
+    for v in range(n):
+      V_ext = []
+      for u in range(n):
+        if (u > v) and (G[v][u] == 1): 
+          V_ext.append(u)
+      ExtendSubgraph([v], V_ext, v, G, k)
+
+    return subgraphs
+
+def ExtendSubgraph(V_sub, V_ext, v, G, k):
+  n = len(G)
+
+  if len(V_sub) == k:
+    print("V_sub, len = k : " + str(V_sub))
+    subgraphs.append(V_sub) 
+    return
+
+  while len(V_ext) != 0:
+    w = V_ext.pop(0)
+    
+    N_w = []
+    for i in range(n):
+      if (G[w][i] == 1): 
+        N_w.append(i)
+
+    N_V_sub = []
+    for i in V_sub:
+      for j in range(n):
+        if (G[i][j] == 1) and (j not in N_V_sub): 
+          N_V_sub.append(j)
+
+    N_excl = np.setdiff1d(N_w, (np.union1d(V_sub, N_V_sub)))
+
+    V_ext_prime = V_ext
+    for u in N_excl:
+      if (u > v) and (u not in V_ext_prime):
+        V_ext_prime.append(u)
+
+    debug = False
+    if debug:
+      print("v : " + str(v))
+      print("w : " + str(w))
+      print("V_sub : " + str(V_sub))
+      print("N_w : " + str(N_w))
+      print("N_V_sub : " + str(N_V_sub))
+      print("N_excl : " + str(N_excl))
+      print("V_ext : " + str(V_ext))
+      print("V_ext_prime : " + str(V_ext_prime))
+      print("---------")
+
+    ExtendSubgraph(np.union1d(V_sub, w), V_ext_prime, v, G, k)
+  return
+
+
--- src/test.py
+++ src/test.py
@@ -3,6 +3,7 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 import utils
+import motifs
 
 data_folder_path = '../dataset/'
 dataset = 'bezerra-et-al-2009_MOD.xls'
@@ -19,3 +20,35 @@ print(cc)
 bc = nx.betweenness_centrality(G, weight=None)
 print("\nBetwenness centrality:")
 print(bc)
+
+
+''' to fix, infinite loop with esu algorithm
+##############################################################
+# generate the adjacency matrix
+G_adj = nx.to_numpy_array(G)
+for i in range(G_adj.shape[0]):
+	for j in range (G_adj.shape[1]):
+		if G_adj[i][j]:
+			G_adj[i][j] = 1
+
+
+#np.savetxt('adj.txt', pd.DataFrame(G_adj).values, fmt='%d') # to check if G_adj is actually an adjacency matrix
+k = 2
+print("ESU:")
+subgraphs = motifs.EnumerateSubgraphs(G_adj, k)
+#subgraphs = motifs.iterative_ESU(G_adj, k)
+
+
+# plot a graphlet
+index = 0
+bi_partition_index = 12 # maximum node of the first partition of the bipartite graph
+
+g = nx.Graph()
+g.add_nodes_from(subgraphs[index][np.where(subgraphs[index]<=bi_partition_index)], bipartite=0)
+g.add_nodes_from(subgraphs[index][np.where(subgraphs[index]>bi_partition_index)], bipartite=1)
+for i in range(k):
+	for j in range(k):
+		if (i<j) and (G_adj[subgraphs[index][i]][subgraphs[index][j]] == 1):
+			g.add_edge(subgraphs[index][i], subgraphs[index][j])
+utils.plot_bipartite_graph(g, subgraphs[index][np.where(subgraphs[index]<=bi_partition_index)])
+'''
