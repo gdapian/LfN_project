496c7d82062dd70510c02dd72b13dda9d2901e2c
gdapian <97063775+gdapian@users.noreply.github.com>
2022-02-03 23:14:37 +0100

    added ESU second phase

--- src/test.py
+++ src/test.py
@@ -29,14 +29,15 @@ for i in range(G_adj.shape[0]):
 
 
 #np.savetxt('adj.txt', pd.DataFrame(G_adj).values, fmt='%d') # to check if G_adj is actually an adjacency matrix
-k = 5
-print("ESU:")
+# ESU Algorithm - First Phase
+k = 4
+print("ESU Algorithm:")
 subgraphs = motifs.EnumerateSubgraphs(G_adj, k, True)
 #subgraphs = motifs.iterative_ESU(G_adj, k)
 
 
-# plot a graphlet
-index = 54
+# ESU Algorithm - Second Phase
+index = 0
 bi_partition_index = 12 # maximum node of the first partition of the bipartite graph
 
 g = nx.Graph()
@@ -47,3 +48,36 @@ for i in range(k):
 		if (i<j) and (G_adj[subgraphs[index][i]][subgraphs[index][j]] == 1):
 			g.add_edge(subgraphs[index][i], subgraphs[index][j])
 utils.plot_bipartite_graph(g, subgraphs[index][np.where(subgraphs[index]<=bi_partition_index)])
+
+mtfs = []
+counts = []
+ratio = []
+mtfs.append(g)
+counts.append(1)
+ratio.append(len(subgraphs[index][np.where(subgraphs[index]<=bi_partition_index)]) / len(subgraphs[index][np.where(subgraphs[index]>bi_partition_index)]))
+#this is the ratio between the nodes of one class and those of the other. it is necessary to correct the problem of isomorphic graphs in case of bipartite graphs
+
+
+for index in range(len(subgraphs)):
+	g_current = nx.Graph()
+	g_current.add_nodes_from(subgraphs[index][np.where(subgraphs[index]<=bi_partition_index)], bipartite=0)
+	g_current.add_nodes_from(subgraphs[index][np.where(subgraphs[index]>bi_partition_index)], bipartite=1)
+	for i in range(k):
+		for j in range(k):
+			if (i<j) and (G_adj[subgraphs[index][i]][subgraphs[index][j]] == 1):
+				g_current.add_edge(subgraphs[index][i], subgraphs[index][j])
+	flag = True
+	for i in range(len(mtfs)):
+		GM = nx.algorithms.isomorphism.GraphMatcher(g_current, mtfs[i])
+		ratio_current = len(subgraphs[index][np.where(subgraphs[index]<=bi_partition_index)]) / len(subgraphs[index][np.where(subgraphs[index]>bi_partition_index)])
+		if(GM.is_isomorphic() and (ratio_current == ratio[i])):
+			flag = False
+			counts[i] = counts[i]+1
+			break
+	if flag:
+		mtfs.append(g_current)
+		counts.append(1)
+		ratio.append(ratio_current)
+		utils.plot_bipartite_graph(g_current, subgraphs[index][np.where(subgraphs[index]<=bi_partition_index)])
+
+print(counts)
